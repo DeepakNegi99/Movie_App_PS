86ad93102452b92d54654d027ec00dfc0eb66fee
Deepak Negi <deepak99negi@gmail.com>
2025-10-29 18:14:03 +0530

    Add Home page and set up React Router
    Introduced a new Home page component and updated App.jsx to render Home instead of Navbar directly. Integrated react-router-dom by wrapping the app in BrowserRouter in main.jsx. Updated dependencies to include react-router-dom and upgraded react-dom.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
-  return <Navbar />;
+  return <Home />;
 }
 
 export default App;
